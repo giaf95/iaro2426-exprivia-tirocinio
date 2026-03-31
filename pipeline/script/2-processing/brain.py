b0c6e98c21f778ea6f7e6cf71e7ddff9de192598
LoryFire <lorenzodamore279@gmail.com>
2026-03-31 17:55:51 +0200

    fix: change LLM

--- pipeline/script/2-processing/brain.py
+++ pipeline/script/2-processing/brain.py
@@ -631,8 +631,8 @@ tools = [cerca_catalogo_specifico, cerca_catalogo_generico, cerca_sito_web, cerc
 
 # configurazione LangGraph e LLM
 # parametri aggiunti per limitare i consumi della cpu e della ram
-llm = ChatOllama(model="qwen2.5:7b-instruct-q4_K_M", temperature=0, num_thread=4, num_ctx=1536)
-llm_con_tools = llm.bind_tools(tools)
+llm = ChatOllama(model="ollama pull llama3.1:8b-instruct-q4_K_M", temperature=0, num_thread=4, num_ctx=2048)
+llm_with_tools = llm.bind_tools(tools)
 
 tool_node = ToolNode(tools)
 
