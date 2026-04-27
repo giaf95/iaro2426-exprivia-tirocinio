209b864cbb94caa2792dd07cc6be369e26c69bd3
LoryFire <lorenzodamore279@gmail.com>
2026-04-27 03:12:01 +0200

    fix: new LLM model

--- pipeline/script/2-processing/brain.py
+++ pipeline/script/2-processing/brain.py
@@ -882,7 +882,7 @@ tools = [#cerca_catalogo_specifico,
 
 # configurazione LangGraph e LLM
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash",
+    model="gemini-2.5-flash-lite",
     temperature=0,
     google_api_key=GOOGLE_API_KEY
 )
